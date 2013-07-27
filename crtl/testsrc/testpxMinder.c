6c66e10e94aff16e9513798b553dc7f68ca6cad1
Chris Westin <cwestin@yahoo.com>
2013-07-27 15:58:36 -0700

    mixin aggregation tested

--- crtl/testsrc/testpxMinder.c
+++ crtl/testsrc/testpxMinder.c
@@ -7,6 +7,14 @@
 #include "pxAlloc.h"
 #endif
 
+#ifndef PXALLOCDEBUG_H
+#include "pxAllocDebug.h"
+#endif
+
+#ifndef PXALLOCEXTENT_H
+#include "pxAllocExtent.h"
+#endif
+
 #ifndef PXALLOCSYSTEM_H
 #include "pxAllocSystem.h"
 #endif
@@ -28,7 +36,7 @@
 typedef struct
 {
     pxObjectStruct objectStruct;
-    bool isDestroyed;
+    bool *pIsDestroyed;
 } MyDestroyable;
 
 static void MyDestroyable_destroy(pxObject *pI)
@@ -36,9 +44,9 @@ static void MyDestroyable_destroy(pxObject *pI)
     MyDestroyable *pThis =
         PXINTERFACE_STRUCT(pI, MyDestroyable, objectStruct.pObjectVt);
 
-    if (pThis->isDestroyed)
+    if (*pThis->pIsDestroyed)
         fprintf(stderr, "testpxMinder: destroyable destroyed a second time\n");
-    pThis->isDestroyed = true;
+    *pThis->pIsDestroyed = true;
 }
 
 static const pxObjectLookup myDestroyableTable[] =
@@ -57,14 +65,15 @@ static const pxObjectVt myDestroyableObjectVt =
     MyDestroyable_destroy,
 };
 
-static void MyDestroyableInit(MyDestroyable *pMD)
+static void MyDestroyableInit(MyDestroyable *pMD, bool *pBool)
 {
     pxObjectStructInit(&pMD->objectStruct, &myDestroyableObjectVt, NULL);
-    pMD->isDestroyed = false;
+    pMD->pIsDestroyed = pBool;
+    *pBool = false;
 }
 
 
-static void testpxMinder()
+static void testpxMinder_basic()
 {
     pxAlloc *const pAlloc = pxAllocSystemGet();
     pxMinder *pMinder;
@@ -76,54 +85,117 @@ static void testpxMinder()
     PXOBJECT_destroy(pObject);
 
     MyDestroyable d1, d2;
+    bool b1, b2;
 
     // test adding and destroying a single object
     pMinder = pxMinderCreate(pAlloc, NULL);
-    MyDestroyableInit(&d1);
+    MyDestroyableInit(&d1, &b1);
     PXMINDER_register(pMinder, (pxObject *)&d1.objectStruct.pObjectVt);
 
     pObject = PXINTERFACE_getInterface(pMinder, pxObject);
     PXOBJECT_destroy(pObject);
-    if (!d1.isDestroyed)
+    if (!b1)
         fprintf(stderr, "testpxMinder: did not destroy d1 (%d)\n", __LINE__);
 
     // test adding and destroying more than one object
     pMinder = pxMinderCreate(pAlloc, NULL);
-    MyDestroyableInit(&d1);
+    MyDestroyableInit(&d1, &b1);
     PXMINDER_register(pMinder, (pxObject *)&d1.objectStruct.pObjectVt);
-    MyDestroyableInit(&d2);
+    MyDestroyableInit(&d2, &b2);
     PXMINDER_register(pMinder, (pxObject *)&d2.objectStruct.pObjectVt);
 
     pObject = PXINTERFACE_getInterface(pMinder, pxObject);
     PXOBJECT_destroy(pObject);
-    if (!d1.isDestroyed)
+    if (!b1)
         fprintf(stderr, "testpxMinder: did not destroy d1 (%d)\n", __LINE__);
-    if (!d2.isDestroyed)
+    if (!b2)
         fprintf(stderr, "testpxMinder: did not destroy d2 (%d)\n", __LINE__);
 
     pxObject *pu1;
 
     // test adding multiple objects, but deregistering one before destroying
     pMinder = pxMinderCreate(pAlloc, NULL);
-    MyDestroyableInit(&d1);
+    MyDestroyableInit(&d1, &b1);
     pu1 = PXMINDER_register(pMinder, (pxObject *)&d1.objectStruct.pObjectVt);
-    MyDestroyableInit(&d2);
+    MyDestroyableInit(&d2, &b2);
     PXMINDER_register(pMinder, (pxObject *)&d2.objectStruct.pObjectVt);
 
     PXOBJECT_destroy(pu1); // deregisters d1
 
     pObject = PXINTERFACE_getInterface(pMinder, pxObject);
     PXOBJECT_destroy(pObject);
-    if (d1.isDestroyed)
+    if (b1)
         fprintf(stderr, "testpxMinder: incorrectly destroyed d1 (%d)\n",
                 __LINE__);
-    if (!d2.isDestroyed)
+    if (!b2)
         fprintf(stderr, "testpxMinder: did not destroy d2 (%d)\n", __LINE__);
+}
+
+static MyDestroyable *MyDestroyableCreate(pxAlloc *pArena, bool *pBool)
+{
+    pxMinder *pMinder = PXINTERFACE_getInterface(pArena, pxMinder);
+    if (!pMinder)
+        fprintf(stderr, "unable to find pxMinder\n");
+
+    MyDestroyable *pM = PXALLOC_alloc(pArena, sizeof(MyDestroyable), 0);
+    MyDestroyableInit(pM, pBool);
+    PXMINDER_register(pMinder, (pxObject *)&pM->objectStruct.pObjectVt);
+
+    return pM;
+}
+
+// this really tests mixins
+static void testpxMinder_arena()
+{
+    pxAlloc *const pAllocS = pxAllocSystemGet();
+    pxAlloc *const pAllocD = pxAllocDebugCreate(pAllocS, 0);
+    pxAllocDebug *const pAllocDebug =
+        PXINTERFACE_getInterface(pAllocD, pxAllocDebug);
+
+    pxAlloc *pAlloc;
+    pxMinder *pMinder;
+    pxObject *pObject;
+
+    pAlloc = pxAllocExtentCreate(pAllocD, 1024, NULL);
+    pMinder = pxMinderCreate(pAlloc, (pxInterface *)pAlloc);
+    pObject = PXINTERFACE_getInterface(pAlloc, pxObject);
+
+    // test interface retrievals
+    pxMinder *pM2 = PXINTERFACE_getInterface(pAlloc, pxMinder);
+    if (pM2 != pMinder)
+        fprintf(stderr, "failed to find aggregated interface\n");
+
+    pxAlloc *pA2 = PXINTERFACE_getInterface(pMinder, pxAlloc);
+    if (pA2 != pAlloc)
+        fprintf(stderr, "failed to find owner interface\n");
+
+    pxObject *pO3 = PXINTERFACE_getInterface(pMinder, pxObject);
+    if (pO3 != pObject)
+        fprintf(stderr, "failed to find owner object interface\n");
+
+    PXOBJECT_destroy(pObject);
+    if (!PXALLOCDEBUG_isEmpty(pAllocDebug))
+        fprintf(stderr, "aggregate destruction failed (%d)\n", __LINE__);
+
+    // test destruction of minded object residing in the arena
+    pAlloc = pxAllocExtentCreate(pAllocD, 1024, NULL);
+    pMinder = pxMinderCreate(pAlloc, (pxInterface *)pAlloc);
+    pObject = PXINTERFACE_getInterface(pAlloc, pxObject);
+
+    bool b1 = false;
+    MyDestroyableCreate(pAlloc, &b1);
+
+    PXOBJECT_destroy(pObject);
+    if (!b1)
+        fprintf(stderr, "destruction didn't destroy minded object\n");
+    if (!PXALLOCDEBUG_isEmpty(pAllocDebug))
+        fprintf(stderr, "aggregate destruction failed (%d)\n", __LINE__);
 }
 
 int main(void)
 {
     pxAllocSystemInit();
-    testpxMinder();
+    testpxMinder_basic();
+    testpxMinder_arena();
     return 0;
 }
