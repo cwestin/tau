c5a0f1c6968d1d11ec2edd46529545fd438a07c0
Chris Westin <cwestin@yahoo.com>
2013-07-27 17:01:56 -0700

    hasher implementation

---
+++ crtl/include/pxHasher.h
@@ -0,0 +1,36 @@
+
+#ifndef PXHASHER_H
+#define PXHASHER_H
+
+#ifndef PXINTERFACE_H
+#include "pxInterface.h"
+#endif
+
+#ifndef PX_STDLIB_H
+#include <stdlib.h>
+#define PX_STDLIB_H
+#endif
+
+
+struct pxHasher;
+typedef struct
+{
+    pxInterfaceVt interfaceVt;
+
+    // methods on here are not virtual (i.e., final)
+} pxHasherVt;
+
+typedef struct pxHasher
+{
+    const pxHasherVt *pHasherVt;
+} pxHasher;
+
+const char pxHasherName[];
+
+int PXHASHER_getHash(pxHasher *pI);
+void PXHASHER_hashBytes(pxHasher *pI, const void *p, size_t length);
+
+struct pxAlloc;
+pxHasher *pxHasherCreate(struct pxAlloc *pAlloc, pxInterface *pOwner);
+
+#endif // PXHASHER_H
