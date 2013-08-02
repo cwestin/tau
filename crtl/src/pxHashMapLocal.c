467f810b48bf1109bace730a7d371f018c0f295f
Chris Westin <cwestin@yahoo.com>
2013-08-02 08:10:27 -0700

    hashmap creation

--- crtl/src/pxHashMapLocal.c
+++ crtl/src/pxHashMapLocal.c
@@ -45,7 +45,8 @@ typedef struct
 {
     pxHashMapLocal_bucket *pBucket;
     size_t nBuckets;
-    unsigned nEntries;
+    size_t nEntries;
+    size_t avgBucket;
 
     const pxHashMapVt *pHashMapVt;
     pxObjectStruct objectStruct;
@@ -60,9 +61,10 @@ static bool pxHashMapLocal_isEmpty(pxHashMap *pI)
     return pThis->nEntries == 0;
 }
 
-static void pxHashMapLocal_find(pxHashMapLocal_s *const pThis,
-                                pxHashable *pKey)
+static void pxHashMapLocal_find(
+    pxHashMapLocal_s *const pThis, pxHashable *const pKey)
 {
+    
 }
 
 static pxInterface *pxHashMapLocal_get(pxHashMap *pI, pxHashable *pKey)
@@ -120,12 +122,31 @@ static const pxObjectVt pxHashMapLocalObjectVt =
 pxHashMap *pxHashMapLocalCreate(
     pxAlloc *pAlloc, pxInterface *pOwner, size_t initCap, size_t avgBucket)
 {
-    pxHashMapLocal_s *const pO =
+    pxHashMapLocal_s *const pThis =
         PXALLOC_alloc(pAlloc, sizeof(pxHashMapLocal_s), PXALLOC_F_DIRTY);
-    pO->pHashMapVt = &pxHashMapLocalHashMapVt;
-    pxObjectStructInit(&pO->objectStruct, &pxHashMapLocalObjectVt, pOwner);
+    pThis->pHashMapVt = &pxHashMapLocalHashMapVt;
+    pxObjectStructInit(&pThis->objectStruct, &pxHashMapLocalObjectVt, pOwner);
 
-    // TODO
+    // don't aim for less than one entry per bucket
+    if (avgBucket < 1)
+        avgBucket = 1;
 
-    return (pxHashMap *)&pO->pHashMapVt;
+    // don't aim for fewer than 5 elements to start
+    if (initCap < 5)
+        initCap = 5;
+
+    // figure out the initial number of buckets
+    // use the first power of two greater than or equal to that requested
+    size_t initBucket0 = (initCap + avgBucket - 1) / avgBucket;
+    size_t initBucket = 1;
+    for(; initBucket < initBucket0; initBucket <<= 1)
+        ;
+
+    pThis->pBucket =
+        PXALLOC_alloc(pAlloc, sizeof(pxHashMapLocal_bucket) * initBucket, 0);
+    pThis->nBuckets = initBucket;
+    pThis->nEntries = 0;
+    pThis->avgBucket = avgBucket;
+
+    return (pxHashMap *)&pThis->pHashMapVt;
 }
