919beb6455f4d3f4407ca22644e95fa0012d162b
Chris Westin <cwestin@yahoo.com>
2013-08-11 12:05:05 -0700

    add file headers to .c files

--- crtl/src/pxHashMapLocal.c
+++ crtl/src/pxHashMapLocal.c
@@ -1,6 +1,27 @@
+/*
+  tau - http://github.com/cwestin/tau
+  Copyright 2013 Chris Westin
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
 
 #ifndef PXHASHMAPLOCAL_H
 #include "pxHashMapLocal.h"
+#endif
+
+#ifndef PX_STDLIB_H
+#include <stdlib.h>
+#define PX_STDLIB_H
 #endif
 
 #ifndef PXALLOC_H
@@ -29,11 +50,6 @@
 
 #ifndef PXOBJECT_H
 #include "pxObject.h"
-#endif
-
-#ifndef PX_STDLIB_H
-#include <stdlib.h>
-#define PX_STDLIB_H
 #endif
 
 
